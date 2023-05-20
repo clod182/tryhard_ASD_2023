3848344884d530460272ba7c70a049f50ada26c1
muletto_clod <>
2023-05-20 16:56:43 +0200

    Completed eser4es2 with also the top-down approach and the description for his time complexity

--- 2022_23/Esercitazione4/PerfettamenteBilanciato.cpp
+++ 2022_23/Esercitazione4/PerfettamenteBilanciato.cpp
@@ -10,7 +10,8 @@ vector<string> split(const string &);
 /* #region funzioni MIE es */
 /* #endregion funzioni MIE es */
 
-/* #region funzioni ausiliarie es */
+/* #region es */
+//*****************************************************************************approccio ricorsivo
 bool perfettamenteBilanciatoRecAux(std::vector<int>& arr, int sum, int index) {
   // Caso base: multiinsieme vuoto
   if (sum == 0) {
@@ -31,10 +32,7 @@ bool perfettamenteBilanciatoRecAux(std::vector<int>& arr, int sum, int index) {
   // 2. Escludere l'elemento corrente dalla somma
   return perfettamenteBilanciatoRecAux(arr, sum, index + 1);
 }
-/* #endregion funzioni ausiliarie es */
 
-/* #region es */
-//approccio ricorsivo
 bool perfettamenteBilanciatoRec(std::vector<int>& arr) {
   int sum = 0;
   for (int num : arr) {
@@ -48,16 +46,51 @@ bool perfettamenteBilanciatoRec(std::vector<int>& arr) {
 
   return perfettamenteBilanciatoRecAux(arr, sum / 2, 0);
 }
+  //*****************************************************************************approccio programmazione dinamica top-down
+  bool perfettamenteBilanciatoTopDown(std::vector<int>& arr) {
+    int totalSum = 0;
+    // Calcolo somma tot
+    for (int num : arr) {
+        totalSum += num;
+    }
+    
+    // Verifica se la somma totale è dispari
+    if (totalSum % 2 != 0) {
+        return false;
+    }
+    
+    int targetSum = totalSum / 2;
+    
+    // Creazione di una unordered_set per memorizzare le somme parziali raggiungibili
+    std::unordered_set<int> sums;
+    sums.insert(0);
+    
+    for (int num : arr) {
+        std::unordered_set<int> newSums;
+        for (int sum : sums) {
+            int newSum = sum + num;
+            newSums.insert(newSum);
+        }
+        sums.insert(newSums.begin(), newSums.end());
+    }
+    
+    // Verifica se la somma target è presente nell'insieme delle somme parziali
+    return sums.find(targetSum) != sums.end();
+}
 /* --------------------------------------------------------------------------------------------------------
-La complessità di questa funzione è "O(2^N)" con n numero di elementi del vettore dato in input.
+La complessità di questa funzione è "O(N * 2^N)" con n numero di elementi del vettore dato in input.
 
-...
+All'inizio calcoliamo la somma totale degli elementi dell'array, che richiede "O(N)" tempo.
+
+Poi cro dell'insieme delle somme parziali: L'algoritmo esegue un ciclo esterno sugli elementi dell'array 
+e un ciclo interno sulle somme parziali. Nel caso peggiore, il numero di somme parziali raggiungibili può 
+essere 2^N, dove N è la dimensione dell'array di input. Quindi, il tempo complessivo per la creazione 
+dell'insieme delle somme parziali è O(N * 2^N).
+
+Pertanto la complessità temporale totale dell'algoritmo top-down è "O(N * 2^N)".
 -------------------------------------------------------------------------------------------------------- */
 
 /* #endregion  es */
-
-
-int perfettamenteBilanciatoTopDown(vector<int> &arr);
 
 
 int main()
